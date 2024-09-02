5c59fd571fa215aac07f984195baedb278702fd7
GregwiseNoah <shwingeorge1@gmail.com>
2024-09-02 14:03:46 +0200

    i should figure out stashing via terminal

--- Skeleton/trial.py
+++ Skeleton/trial.py
@@ -54,7 +54,7 @@ class MainWindow(QWidget):
         #self.plot_graph.setYRange(-2100, 2100)
         self.times = np.linspace(0.0019, 0.0026, 16384)
         self.time = list(self.times[68:78])
-        self.fwd_qs = np.load("D:\Pyqt5\Skeleton\Data\q_19.npy")
+        self.fwd_qs = np.load("/home/george/Documents/HZB/PyQt5/Skeleton/Data/q_19.npy")
         self.fwd_q = self.fwd_qs[68:78].tolist()#list(self.fwd_qs[0:10])
 
         # Get a line reference
@@ -91,16 +91,13 @@ class MainWindow(QWidget):
         iq.setLayout(layout) 
 
         fwd_i = pg.PlotWidget()
-        #fwd_i.setBackground("w")
         fwd_i.plot(x,y, pen = 'k')
         
         fwd_q = pg.PlotWidget()
-        fwd_q.setBackground("w")
-        fwd_q.plot(x,np.cos(x))
+        fwd_q.plot(x,np.cos(x), pen = 'k')
         
         fwd_x = pg.PlotWidget()
-        fwd_x.setBackground("w")
-        fwd_x.plot(x[:50],[randint(0,100) for _ in range(50)])
+        fwd_x.plot(x[:50],[randint(0,100) for _ in range(50)], pen = 'k')
         
         layout.addWidget(plot_graph, 0, 0)
         layout.addWidget(fwd_i, 0, 1)
