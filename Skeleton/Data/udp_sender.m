2d22f6a4fe97504b5279db3b9b4bb7b287a3383c
Ashwin V George <62826995+GregwiseNoah@users.noreply.github.com>
2024-10-14 10:47:51 +0200

    matlab sender modified

--- Skeleton/Data/udp_sender.m
+++ Skeleton/Data/udp_sender.m
@@ -1,13 +1,11 @@
 
 serverIP = '127.0.1.1'; 
 serverPort = 5050;     
-
-udpSender = udpport();
-
 loadedData = load('Fwd_I.mat'); 
 
 
-dataToSend = loadedData.Fwd_I.Data(3,:);
+udpSender = udpport();
 
+dataToSend = loadedData.Fwd_I.Data(3,:);
 
 write(udpSender, dataToSend, serverIP, serverPort);
